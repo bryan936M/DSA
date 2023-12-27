2a1296b25f22ef386d15a4f746cc9118f8cf3537
Bryan Murasira <bryanmurasira@gmail.com>
2023-12-27 11:20:59 +0200

    Refactor code to improve readability and add alternative solution for checking balanced parentheses

--- Stack/exercise_2.py
+++ Stack/exercise_2.py
@@ -1,31 +1,53 @@
 from main import Stack
 
-# ---------------------- My Solution ----------------------
+# # ---------------------- My Solution ----------------------
 
-open_parantheses = ['(', '[', '{']
-closed_parantheses = [')', ']', '}']
+# open_parantheses = ['(', '[', '{']
+# closed_parantheses = [')', ']', '}']
 
 
-def is_balanced(string):
-  s = Stack()
+# def is_balanced(string):
+#   s = Stack()
   
-  for char in string:
+#   for char in string:
     
-    if char in open_parantheses:
-      s.push(char)
+#     if char in open_parantheses:
+#       s.push(char)
         
-    if char in closed_parantheses:
-      if s.is_empty():
-        return False
+#     if char in closed_parantheses:
+#       if s.is_empty():
+#         return False
       
-      top = s.pop()
+#       top = s.pop()
       
-      if closed_parantheses.index(char) != open_parantheses.index(top):
-        return False
+#       if closed_parantheses.index(char) != open_parantheses.index(top):
+#         return False
       
-  return s.is_empty()
+#   return s.is_empty()
+
+# ---------------------- Another Solution ----------------------
+
+def is_match(ch1, ch2):
+    match_dict = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    return match_dict[ch1] == ch2
 
 
+def is_balanced(s):
+    stack = Stack()
+    for ch in s:
+        if ch=='(' or ch=='{' or ch == '[':
+            stack.push(ch)
+        if ch==')' or ch=='}' or ch == ']':
+            if stack.size()==0:
+                return False
+            if not is_match(ch,stack.pop()):
+                return False
+
+    return stack.size()==0
 
 
 
